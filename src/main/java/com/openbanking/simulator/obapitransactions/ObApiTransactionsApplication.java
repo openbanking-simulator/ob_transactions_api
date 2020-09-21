d112fa40308af7adb7f2d8e8bb9ec5d729dbeb16
Madan Narra <narra.madan@outlook.com>
2020-09-22 07:40:03 +1000

    Initial Commit

---
+++ src/main/java/com/openbanking/simulator/obapitransactions/ObApiTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapitransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObApiTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObApiTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openbanking/simulator/obapitransactions/ObApiTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapitransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObApiTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
